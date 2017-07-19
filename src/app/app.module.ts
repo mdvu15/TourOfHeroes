1ede6323e7ca07efd0567073c52e1581b747dd17
Minh Vu <mdvu15@earlham.edu>
2017-07-19 00:27:16 -0400

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule /** <-- import the FormsModule before binding with 
+    FormsModule, /** <-- import the FormsModule before binding with 
 [(ngModel)] */
+	RouterModule.forRoot([
+		{
+			path: 'heroes',
+			component: HeroesComponent
+		}
+	])
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
