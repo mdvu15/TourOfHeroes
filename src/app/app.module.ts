d9dfdd555fc7b987f62763a92dfbfb9b9a26c6d3
Minh Vu <mdvu15@earlham.edu>
2017-07-23 00:49:00 -0400

    http requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'; 
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -19,9 +23,10 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule, /** <-- import the FormsModule before binding with 
-[(ngModel)] */
-	AppRoutingModule
+    FormsModule, /* <-- import the FormsModule before binding with [(ngModel)] */
+	  AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [HeroService], //set HeroService available for all components
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,19 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-hero';
+
 
 @Injectable()
 export class HeroService{
-	getHeroes(): Promise<Hero[]> {  /*generates a promise object that can resolve 
-		to an array of hero objects */
-		return Promise.resolve(HEROES);
-	}
+    private heroesUrl = 'api/heroes';
 
-	getHero(id: number): Promise<Hero> {
-		return this.getHeroes()
-					.then(heroes => heroes.find(hero => hero.id === id));
-	//filters the heoroes list (from getHeroes) by id
-	}
+    constructor(private http: Http) {}
+
+    getHeroes(): Promise<Hero[]> {  /*generates a promise object that can resolve 
+        to an array of hero objects */
+        return this.http.get(this.heroesUrl) //http.get returns an Observable
+        .toPromise()
+        .then(response => response.json().data as
+            Hero[])
+        .catch(this.handleError); //returns a promise resolving to the return value of the callback
+    }
+
+    getHero(id: number): Promise<Hero> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get(url)
+                .toPromise()
+                .then(response => response.json().data as Hero)
+                .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error); //return a promise object rejected with a given reason
+    }
+
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http.put(url, JSON.stringify(hero), {headers: this.headers})
+                .toPromise()
+                .then(() => hero)
+                .catch(this.handleError);
+    }
 }
 
